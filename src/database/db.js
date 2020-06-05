6d008c211f871ea4145b73bd9586966fe06b0b72
Stefano Paulo <stefanopaulo47@gmail.com>
2020-06-05 11:05:04 -0300

    criando e inserindo dados na tabela

--- src/database/db.js
+++ src/database/db.js
@@ -7,4 +7,54 @@ const db  = new sqlite3.Database("./src/database/database.db")
 // utilizar o obejto de banco de dados para nossas operações
 db.serialize(() => {
     // criar uma tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+
+    // inserir dados na tabela
+    const query = `
+        INSERT INTO places (
+            image, 
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
+        "Colectoria",
+        "Guilherme Gembala, Jardim América",
+        "N° 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos Eletrônicos, Lâmpadas"
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+
+    db.run(query, values, afterInsertData)
+
+    // consultar os dados da tabela
+
+    // deletar dados da tabela
 })
